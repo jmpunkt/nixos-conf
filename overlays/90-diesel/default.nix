8225fbbbf96298da0c3aa56c218ea7e5fb7512c2
Jonas Meurer <jmpunkt@outlook.com>
2019-06-26 00:52:27 +0200

    Cleaned up diesel_cli overlay

--- overlays/90-diesel/default.nix
+++ overlays/90-diesel/default.nix
@@ -2,11 +2,6 @@ self: super:
 
 let
   crates = import ./Cargo.nix;
-  crates-io = import ./crates-io.nix;
-  # features = (crates-io.features_.diesel_cli."1.4.0" crates.deps {
-  #       features."diesel_cli"."1.4.0".deault = false;
-  #       features."diesel_cli"."1.4.0".postgres = true;
-  # });
 in
   {
     diesel_cli = (crates.diesel_cli { }).override {
@@ -16,9 +11,6 @@ in
       rust = self.latest.rustChannels.stable.rust;
       crateOverrides = super.defaultCrateOverrides // {
         diesel = attrs: {
-          # features = [
-          #   "url" "sqlite" "postgres" "default"
-          # ];
           buildInputs = [
             super.pkgs.postgresql_11
             super.pkgs.sqlite

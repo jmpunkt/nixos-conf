844e0bca664c497ec1a34ac97191ffa293c25b6f
Jonas Meurer <jmpunkt@outlook.com>
2019-08-01 17:17:59 +0200

    Added multiple rust targets

--- overlays/40-toolchains.nix
+++ overlays/40-toolchains.nix
@@ -2,6 +2,11 @@ self: super:
 
 {
   rustToolchain = super.latest.rustChannels.stable.rust.override {
+    targets = [
+      "x86_64-unknown-linux-gnu"
+      "x86_64-unknown-linux-musl"
+      "wasm32-unknown-unknown"
+    ];
     extensions = [
       "rust-src"
       "rls-preview"

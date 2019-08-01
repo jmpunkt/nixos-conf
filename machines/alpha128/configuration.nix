5a12d8d94789853c4dddda894a1f6cb6ceae690e
Jonas Meurer <jmpunkt@outlook.com>
2019-08-01 17:16:07 +0200

    Added configuration for new machine

---
+++ machines/alpha128/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, options, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../configurations/base.nix
+    ./../../configurations/kde.nix
+    ./../../configurations/locale.nix
+    ./../../configurations/misc.nix
+    ./../../configurations/network.nix
+    ./../../configurations/shell.nix
+    ./../../configurations/tmux.nix
+    ./../../configurations/users/jonas.nix
+  ];
+
+  nix.nixPath = options.nix.nixPath.default ++
+  [ "nixpkgs-overlays=/etc/nixos/nixos-conf/overlays" ];
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.cleanTmpDir = true;
+
+  networking.hostName = "alpha128";
+
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.accelProfile = "flat";
+
+  system.stateVersion = "19.03";
+}

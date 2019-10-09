a01603e38d135b0a2c120bf340f699563f08fd15
Jonas Meurer <jmpunkt@outlook.com>
2019-10-09 19:55:03 +0200

    Fixed deprecated options (neovim)

--- home/jonas/neovim/default.nix
+++ home/jonas/neovim/default.nix
@@ -7,40 +7,37 @@
     enable = true;
 
     withPython3 = true;
+    extraConfig = builtins.readFile ./neovim.vim;
+    plugins = with pkgs.vimPlugins; [
+      fzfWrapper
+      fzf-vim
+      vim-polyglot
+      vimtex
+      vim-cursorword
+      vim-commentary
+      denite-nvim
+      vim-fugitive
+      vim-airline
+      vim-airline-themes
+      vim-signature
+      tagbar
+      base16-vim
+      vim-nix
+      vim-sensible
+      nerdtree
+      nerdtree-git-plugin
 
-    configure = {
-      customRC = builtins.readFile ./neovim.vim;
-      plug.plugins = with pkgs.vimPlugins; [
-        fzfWrapper
-        fzf-vim
-        vim-polyglot
-        vimtex
-        vim-cursorword
-        vim-commentary
-        denite-nvim
-        vim-fugitive
-        vim-airline
-        vim-airline-themes
-        vim-signature
-        tagbar
-        base16-vim
-        vim-nix
-        vim-sensible
-        nerdtree
-        nerdtree-git-plugin
-
-        coc-nvim
-        coc-rls
-        coc-python
-        coc-yaml
-        coc-vimtex
-        coc-json
-        coc-css
-        lexima
-        quick-scope
-        vim-searchindex
-        vim-better-whitespace
-      ];
-    };
+      coc-nvim
+      coc-rls
+      coc-python
+      coc-yaml
+      coc-vimtex
+      coc-json
+      coc-css
+      lexima
+      quick-scope
+      vim-searchindex
+      vim-better-whitespace
+    ];
   };
 }

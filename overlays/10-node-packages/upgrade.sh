592e183441f70aa9c45837c6c364a44e25034542
Jonas Meurer <jmpunkt@outlook.com>
2019-10-27 01:43:31 +0200

    Removed old JS packages

--- overlays/10-node-packages/upgrade.sh
+++ overlays/10-node-packages/upgrade.sh
@@ -1,7 +1,7 @@
 # /bin/sh
 # run inside nix-shell -p nodePackages.node2nix
 
-cmd="node2nix -8 -i packages.json -c plugins.nix"
+cmd="node2nix -10 -i packages.json -c plugins.nix"
 
 if ! type "node2nix" &> /dev/null; then
     nix-shell -p nodePackages.node2nix --run "$cmd"

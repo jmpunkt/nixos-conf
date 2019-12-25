64e20291262d7b7928289d9119bc09adf6d159f4
Jonas Meurer <jmpunkt@outlook.com>
2019-12-25 19:11:37 +0100

    Removed osrm-data since archives are time limited

--- overlays/10-pkgs.nix
+++ overlays/10-pkgs.nix
@@ -3,7 +3,6 @@ self: super:
 rec {
   nyan-project = super.callPackage ../pkgs/nyan-project/default.nix {};
   openage = super.callPackage ../pkgs/openage/default.nix {};
-  orsm-data = super.callPackage ../pkgs/osrm-data/default.nix {};
   gluon-lsp = super.callPackage ../pkgs/gluon-lsp/default.nix {};
   gluon = super.callPackage ../pkgs/gluon-repl/default.nix {};
 }

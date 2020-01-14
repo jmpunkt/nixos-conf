88c743feef00d29bfc83676885f84c986eed48e6
Jonas Meurer <jmpunkt@outlook.com>
2020-01-14 14:54:40 +0100

    Added VCMI Project to packages

--- overlays/10-pkgs.nix
+++ overlays/10-pkgs.nix
@@ -3,6 +3,7 @@ self: super:
 rec {
   nyan-project = super.callPackage ../pkgs/nyan-project/default.nix {};
   openage = super.callPackage ../pkgs/openage/default.nix {};
+  vcmi = super.callPackage ../pkgs/vcmi/default.nix {};
   gluon-lsp = super.callPackage ../pkgs/gluon-lsp/default.nix {};
   gluon = super.callPackage ../pkgs/gluon-repl/default.nix {};
 }

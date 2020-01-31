1616c434c8c9a77a1699b4cb2430e0e7f330ade4
Jonas Meurer <jmpunkt@outlook.com>
2020-01-31 23:18:58 +0100

    Changed prefix for custom packages

--- overlays/10-pkgs.nix
+++ overlays/10-pkgs.nix
@@ -1,15 +1,19 @@
 self: super:
 
 {
+  # do not export libraries with prefix
   nyan-project = super.callPackage ../pkgs/nyan-project/default.nix {};
-  openage = super.callPackage ../pkgs/openage/default.nix {};
   vcmi = super.callPackage ../pkgs/vcmi/default.nix {};
-  opmon = super.callPackage ../pkgs/opmon/default.nix {};
-  python3Packages = {
-    netira = super.callPackage ../pkgs/netira/default.nix {};
-  } // super.python3Packages;
-  tuxemon = super.callPackage ../pkgs/tuxemon/default.nix {};
-  gluon-lsp = super.callPackage ../pkgs/gluon-lsp/default.nix {};
-  gluon = super.callPackage ../pkgs/gluon-repl/default.nix {};
-  rust-analyzer = super.callPackage ../pkgs/rust-analyzer/default.nix {};
+
+  jmpunkt = (super.jmpunkt or {}) // {
+    openage = super.callPackage ../pkgs/openage/default.nix {};
+    opmon = super.callPackage ../pkgs/opmon/default.nix {};
+    python3Packages = (super.python3Packages or {}) // {
+      netira = super.callPackage ../pkgs/netira/default.nix {};
+    };
+    tuxemon = super.callPackage ../pkgs/tuxemon/default.nix {};
+    gluon-lsp = super.callPackage ../pkgs/gluon-lsp/default.nix {};
+    gluon = super.callPackage ../pkgs/gluon-repl/default.nix {};
+    rust-analyzer = super.callPackage ../pkgs/rust-analyzer/default.nix {};
+  };
 }

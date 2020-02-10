e5cb3a24ce15948db9ae443cc3f73f7fdb7689f7
Jonas Meurer <jmpunkt@outlook.com>
2020-02-10 21:39:25 +0100

    Updated rust-analyzer and fixed not found error.
    Added Cargo from the given rust platform as a run-time
    dependency. Rust-analyzer could not find the cargo executable although
    Emacs finds cargo (with executable-find). Now the rust-analyzer is
    wrapped and the environment variable PATH is extended with the cargo
    path.

--- pkgs/rust-analyzer/default.nix
+++ pkgs/rust-analyzer/default.nix
@@ -1,31 +1,38 @@
-{ stdenv, mozillaRustPlatform, fetchFromGitHub }:
+{ stdenv, mozillaRustPlatform, fetchFromGitHub, makeWrapper }:
 
 mozillaRustPlatform.buildRustPackage rec {
   pname = "rust-analyzer";
-  version = "2020-01-20";
+  version = "2020-02-10";
 
   src = fetchFromGitHub {
     owner = "rust-analyzer";
     repo = "rust-analyzer";
     rev = "tags/${version}";
-    sha256 = "1dx2xlbmymd2wgizcp3v98nxrfj1fw74jwywq3b2fsw0gf3bx1la";
+    sha256 = "1iz40iq7xga3rg3ps8m6g5jc899q822j7j43041x89kd96gqg6cl";
   };
+
+  nativeBuildInputs = [ makeWrapper ];
 
   # Remove usage of rustup for the `codegen` path.
   patches = [ ./use-rustfmt.patch ];
 
   # Exclude xtask since it depends on rustup
-  cargoBuildFlags = ["--bin ra_cli" "--bin ra_lsp_server"];
+  cargoBuildFlags = [ "--bin ra_cli" "--bin ra_lsp_server" ];
 
   preBuild = ''
     cargo xtask codegen
   '';
 
-  cargoSha256 = "0qsil1y2gv8h75x3mnp6h7b295k3icwjhmhriy3bv49qm11ffz0w";
+  postInstall = ''
+    wrapProgram $out/bin/ra_cli --set "PATH" "$PATH:${mozillaRustPlatform.rust.cargo}/bin/"
+    wrapProgram $out/bin/ra_lsp_server --set "PATH" "$PATH:${mozillaRustPlatform.rust.cargo}/bin/"
+  '';
+
+  cargoSha256 = "1n4d2a2q2z87kw2f0wnc2yqd12w19xpcrkldf6yp428s6fqp5nhp";
 
   meta = with stdenv.lib; {
     description = "An experimental Rust compiler front-end for IDEs";
-    homepage = https://github.com/rust-analyzer/rust-analyzer;
+    homepage = "https://github.com/rust-analyzer/rust-analyzer";
     license = [ licenses.mit licenses.asl20 ];
     maintainers = [ ];
   };

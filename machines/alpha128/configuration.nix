48b87ad168eaf2678c4dc91722131cafa7f9737a
Jonas Meurer <jmpunkt@outlook.com>
2020-04-05 15:58:52 +0200

    Prevent crashes and errors (Ryzen CPU)

--- machines/alpha128/configuration.nix
+++ machines/alpha128/configuration.nix
@@ -18,6 +18,11 @@
       efi.efiSysMountPoint = "/boot/efi";
     };
     cleanTmpDir = true;
+    blacklistedKernelModules = [
+      # Disable: https://bbs.archlinux.org/viewtopic.php?id=239075
+      "sp5100_tco"
+    ];
+    kernelParams = [ "processor.max_cstate=1" ];
   };
 
   networking.hostName = "alpha128";

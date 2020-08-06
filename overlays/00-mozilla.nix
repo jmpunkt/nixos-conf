d233fc3250b4eac72604dcb65064dbf94089de9b
Jonas Meurer <jmpunkt@outlook.com>
2020-08-06 20:13:12 +0200

    [OVERLAY] updated Mozilla overlay

--- overlays/00-mozilla.nix
+++ overlays/00-mozilla.nix
@@ -4,7 +4,7 @@ with super.lib;
 let
   mozillaTarball = builtins.fetchGit {
     url = "https://github.com/mozilla/nixpkgs-mozilla";
-    rev = "e912ed483e980dfb4666ae0ed17845c4220e5e7c";
+    rev = "efda5b357451dbb0431f983cca679ae3cd9b9829";
   };
   overlay = self: super: { inherit mozillaTarball; };
 in foldl' (flip extends) (_: super) [ (import mozillaTarball) overlay ] self

58d34a4030888d7a3d8d7656debd584379bb11cc
Jonas Meurer <jmpunkt@outlook.com>
2020-08-24 14:05:49 +0200

    [OVERLAY] disabled Discord overwrite

--- overlays/00-patch.nix
+++ overlays/00-patch.nix
@@ -3,12 +3,13 @@ self: super:
 # contains patches for machine configuration, normally the overlay is
 # not shared with the machine configuration.
 {
-  discord = super.discord.overrideAttrs (old: rec {
-    version = "0.0.11";
-    src = super.fetchurl {
-      url =
-        "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "1saqwigi1gjgy4q8rgnwyni57aaszi0w9vqssgyvfgzff8fpcx54";
-    };
-  });
+  # NOTICE: disabled until discord breaks again
+  # discord = super.discord.overrideAttrs (old: rec {
+  #   version = "0.0.11";
+  #   src = super.fetchurl {
+  #     url =
+  #       "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+  #     sha256 = "1saqwigi1gjgy4q8rgnwyni57aaszi0w9vqssgyvfgzff8fpcx54";
+  #   };
+  # });
 }

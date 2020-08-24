79dab61ecec7cb8b7b4bf995a280e688b1836eb2
Jonas Meurer <jmpunkt@outlook.com>
2020-08-24 14:05:10 +0200

    [OVERLAY] updated Emacs overlay

--- overlays/00-emacs.nix
+++ overlays/00-emacs.nix
@@ -4,7 +4,7 @@ with super.lib;
 let
   mozillaTarball = builtins.fetchGit {
     url = "https://github.com/nix-community/emacs-overlay";
-    rev = "80e5a71f67da299dba1b3c4e00c9f16ba539766f";
+    rev = "2d1b44e897716fc7c75b0f674193740b45cafd62";
   };
   overlay = self: super: { inherit mozillaTarball; };
 in foldl' (flip extends) (_: super) [ (import mozillaTarball) overlay ] self

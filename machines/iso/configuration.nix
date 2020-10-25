7abe1f10ab415d2cd1c4eb0dfc6cd66c2e845cd3
Jonas Meurer <jmpunkt@outlook.com>
2020-10-25 16:03:47 +0100

    [ISO] added ISO configuration

---
+++ machines/iso/configuration.nix
@@ -0,0 +1,39 @@
+({ modulesPath, config, pkgs, ... }: {
+
+  imports = [
+    "${modulesPath}/installer/cd-dvd/channel.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
+  ];
+
+  isoImage.volumeID =
+    "nixos-${config.system.nixos.release}-${pkgs.stdenv.hostPlatform.uname.processor}";
+
+  environment.systemPackages = with pkgs; [
+    chromium
+    gparted
+    neovim
+    alacritty
+  ];
+
+  programs.fish.enable = true;
+
+  services.xserver = {
+    desktopManager.xfce.enable = true;
+    displayManager = {
+      lightdm = {
+        enable = true;
+        autoLogin = {
+          enable = true;
+          user = "nixos";
+        };
+      };
+    };
+  };
+
+  services.ntp.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  services.xserver.layout = "de";
+  console.keyMap = "de";
+})

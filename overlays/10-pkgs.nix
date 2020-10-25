e18506ef8d3417aac8ff9d81db96a94a31ffd74c
Jonas Meurer <jmpunkt@outlook.com>
2020-10-25 16:00:42 +0100

    [PKG] changed location of package definition

--- overlays/10-pkgs.nix
+++ overlays/10-pkgs.nix
@@ -1,21 +1,9 @@
 self: super:
 
+# NOTICE: export everything with a prefix, making third-party
+# dependencies immediately visible
 {
-  # export everything with a prefix, to mark missing dependencies inside the nix files
-  jmpunkt = (super.jmpunkt or { }) // rec {
-    nyan-project = super.callPackage ../pkgs/games/nyan-project { };
-    vcmi = super.callPackage ../pkgs/games/vcmi { };
-    veloren = super.callPackage ../pkgs/games/veloren { };
-    openage = super.callPackage ../pkgs/games/openage { };
-    opmon = super.callPackage ../pkgs/games/opmon { };
-    tuxemon = super.callPackage ../pkgs/games/tuxemon { };
-    gluon-lsp = super.callPackage ../pkgs/misc/gluon-lsp { };
-    gluon = super.callPackage ../pkgs/misc/gluon-repl { };
-    papyrus-rt = super.callPackage ../pkgs/applications/papyrus-rt { };
-    gamemode = super.callPackage ../pkgs/misc/gamemode { };
-    inih = super.callPackage ../pkgs/misc/inih { };
-    kaffeine = super.callPackage ../pkgs/applications/kaffeine { };
-  };
+  jmpunkt = (super.jmpunkt or { }) // (super.callPackage ../pkgs { });
 
   python3Packages = (super.python3Packages or { }) // {
     jmpunkt = super.callPackage ../pkgs/python3Packages { };
---
+++ pkgs/default.nix
@@ -0,0 +1,16 @@
+{ callPackage }:
+
+{
+  nyan-project = callPackage ../pkgs/games/nyan-project { };
+  vcmi = callPackage ../pkgs/games/vcmi { };
+  veloren = callPackage ../pkgs/games/veloren { };
+  openage = callPackage ../pkgs/games/openage { };
+  opmon = callPackage ../pkgs/games/opmon { };
+  tuxemon = callPackage ../pkgs/games/tuxemon { };
+  gluon-lsp = callPackage ../pkgs/misc/gluon-lsp { };
+  gluon = callPackage ../pkgs/misc/gluon-repl { };
+  papyrus-rt = callPackage ../pkgs/applications/papyrus-rt { };
+  gamemode = callPackage ../pkgs/misc/gamemode { };
+  inih = callPackage ../pkgs/misc/inih { };
+  kaffeine = callPackage ../pkgs/applications/kaffeine { };
+}

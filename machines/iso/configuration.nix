ed0ce6bbb864124f1dcee074ba66edb836be3139
Jonas Meurer <jmpunkt@outlook.com>
2020-10-30 21:55:22 +0100

    [ISO] ISO can be built on 32-bit systems without Firefox

--- machines/iso/configuration.nix
+++ machines/iso/configuration.nix
@@ -1,31 +1,46 @@
-({ modulesPath, config, pkgs, ... }: {
+{ modulesPath, config, lib, pkgs, ... }:
 
+{
   imports = [
     "${modulesPath}/installer/cd-dvd/channel.nix"
-    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-base.nix"
   ];
 
   isoImage.volumeID =
     "nixos-${config.system.nixos.release}-${pkgs.stdenv.hostPlatform.uname.processor}";
 
   environment.systemPackages = with pkgs; [
-    chromium
+    ungoogled-chromium
     gparted
     neovim
     alacritty
   ];
 
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = lib.mkForce false;
+
+  powerManagement.enable = true;
+
+  hardware.pulseaudio.enable = true;
+
   programs.fish.enable = true;
 
   services.xserver = {
+    enable = true;
     desktopManager.xfce.enable = true;
     displayManager = {
-      lightdm = {
+      lightdm.enable = true;
+      autoLogin = {
         enable = true;
-        autoLogin = {
-          enable = true;
-          user = "nixos";
-        };
+        user = "nixos";
       };
     };
   };
@@ -36,4 +51,4 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   services.xserver.layout = "de";
   console.keyMap = "de";
-})
+}

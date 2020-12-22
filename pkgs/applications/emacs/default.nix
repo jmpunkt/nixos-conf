22e0e7b94092e28b4199bc344e2bfeb72d36539a
Jonas Meurer <jmpunkt@outlook.com>
2020-12-22 10:32:27 +0100

    [PKGS] build eslint extension from source

--- pkgs/applications/emacs/default.nix
+++ pkgs/applications/emacs/default.nix
@@ -8,22 +8,33 @@
 , nodePackages
 , vscode-utils
 , nodejs
+, fetchFromGitHub
 }:
 let
   emacs = emacsGcc;
 
-  vscode-eslint = vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-eslint";
-      publisher = "dbaeumer";
-      # https://github.com/emacs-lsp/lsp-mode/issues/1932#issuecomment-714633460
-      version = "2.1.8";
-      sha256 = "parXZhF9qyRAlmeGItCbvPfyyQQ9WmlBKKFYQ8KIFH0=";
+  vscode-eslint = let
+    name = "vscode-eslint";
+    publisher = "dbaeumer";
+    # https://github.com/emacs-lsp/lsp-mode/issues/1932#issuecomment-714633460
+    version = "2.1.8";
+  in
+    vscode-utils.buildVscodeExtension {
+      name = "${publisher}-${name}-${version}";
+
+      vscodeExtUniqueId = "${publisher}.${name}";
+
+      src = fetchFromGitHub {
+        owner = "microsoft";
+        repo = "vscode-eslint";
+        rev = "release/${version}";
+        sha256 = "ceNiSXlyeglL9ML73iSTttOEy2rYyc3/lPGOhnL1ZoU=";
+      };
+
+      meta = {
+        license = stdenv.lib.licenses.mit;
+      };
     };
-    meta = {
-      license = stdenv.lib.licenses.mit;
-    };
-  };
 
   pathOfExtension = ext: "${ext}/share/vscode/extensions/${ext.vscodeExtUniqueId}";
 in

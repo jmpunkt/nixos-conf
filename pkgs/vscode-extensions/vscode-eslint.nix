cf52352fe4f31f1c522b527f696baa739272d57b
Jonas Meurer <jmpunkt@outlook.com>
2020-12-24 11:04:35 +0100

    [PKGS] fetch vscode-eslint from store

--- pkgs/vscode-extensions/vscode-eslint.nix
+++ pkgs/vscode-extensions/vscode-eslint.nix
@@ -1,21 +1,11 @@
 { lib, vscode-utils, nodejs, fetchFromGitHub }:
 
-let
-  name = "vscode-eslint";
-  publisher = "dbaeumer";
-  # https://github.com/emacs-lsp/lsp-mode/issues/1932#issuecomment-714633460
-  version = "2.1.8";
-in
-vscode-utils.buildVscodeExtension {
-  name = "${publisher}-${name}-${version}";
-
-  vscodeExtUniqueId = "${publisher}.${name}";
-
-  src = fetchFromGitHub {
-    owner = "microsoft";
-    repo = "vscode-eslint";
-    rev = "release/${version}";
-    sha256 = "ceNiSXlyeglL9ML73iSTttOEy2rYyc3/lPGOhnL1ZoU=";
+vscode-utils.buildVscodeMarketplaceExtension {
+  mktplcRef = {
+    name = "vscode-eslint";
+    publisher = "dbaeumer";
+    version = "2.1.8";
+    sha256 = "parXZhF9qyRAlmeGItCbvPfyyQQ9WmlBKKFYQ8KIFH0=";
   };
 
   meta = {

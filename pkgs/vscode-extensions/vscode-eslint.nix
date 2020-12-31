14dc40947eee3f3a3d9f44a7ef3849572bac80bd
Jonas Meurer <jmpunkt@outlook.com>
2020-12-31 15:53:19 +0100

    [PKGS] update eslint vscode

--- pkgs/vscode-extensions/vscode-eslint.nix
+++ pkgs/vscode-extensions/vscode-eslint.nix
@@ -4,8 +4,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "vscode-eslint";
     publisher = "dbaeumer";
-    version = "2.1.8";
-    sha256 = "SNAcFeD0ZIr6FTUC2CPdaVqRA0TcYcfK24tT7wUL3KM=";
+    version = "2.1.14";
+    sha256 = "bVGmp871yu1Llr3uJ+CCosDsrxJtD4b1+CR+omMUfIQ=";
   };
 
   meta = {

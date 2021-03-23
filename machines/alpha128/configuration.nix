e813aee1ae98ef0bf09b8738aa8c9778fcb61458
Jonas Meurer <jmpunkt@outlook.com>
2021-03-23 19:25:28 +0100

    [CONF] set amdgpu profile to low for desktop use

--- machines/alpha128/configuration.nix
+++ machines/alpha128/configuration.nix
@@ -52,5 +52,23 @@
 
   services.xserver.videoDrivers = [ "amdgpu" ];
 
+  systemd.services.amdgpu-profile-low =
+    {
+      description = "Sets the power profile to low within the AMDGPU driver.";
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = pkgs.writers.writeBash "enable-profile" ''
+          echo "low" > /sys/class/drm/card0/device/power_dpm_force_performance_level
+        '';
+        ExecStop = pkgs.writers.writeBash "disable-profile" ''
+          echo "auto" > /sys/class/drm/card0/device/power_dpm_force_performance_level
+        '';
+        RemainAfterExit = "yes";
+      };
+    };
+
   system.stateVersion = "20.03";
 }

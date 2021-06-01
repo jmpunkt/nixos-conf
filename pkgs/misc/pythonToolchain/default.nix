6b353d66ae7fb37f6b6c167c55db8d5fd2d3514e
Jonas Meurer <jmpunkt@outlook.com>
2021-06-01 15:27:44 +0200

    [PKGS] removed unused python packages in python environment

--- pkgs/misc/pythonToolchain/default.nix
+++ pkgs/misc/pythonToolchain/default.nix
@@ -1,22 +1,8 @@
 { python37 }:
 
-python37.withPackages (ps:
-  with ps; [
-    python-language-server
-    pyls-black
-    pyls-mypy
-    jedi
-    epc
-
-    mccabe
-    mypy
-    pylama
-    black
-    isort
-    pycodestyle
-    pyflakes
-    yapf
-
-    numpy
-    scipy
-  ])
+python37.withPackages (
+  ps:
+    with ps; [
+      jedi
+    ]
+)

2816e3588091302f180c2e0f231f8873016c575e
Jonas Meurer <jmpunkt@outlook.com>
2021-06-03 19:43:08 +0200

    [PKGS] fix overlay

--- overlays/10-pkgs.nix
+++ overlays/10-pkgs.nix
@@ -9,10 +9,6 @@ self: super:
     jmpunkt = super.callPackage ../pkgs/python3Packages {};
   };
 
-  nodePackages = (super.nodePackages or {}) // {
-    jmpunkt = super.callPackage ../pkgs/nodePackages {};
-  };
-
   vimPlugins = (super.vimPlugins or {}) // {
     jmpunkt = super.callPackage ../pkgs/vimPlugins {};
   };

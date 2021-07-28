4bc68e16d1712f7e48c474c30b3814f6149e91f9
Jonas Meurer <jmpunkt@outlook.com>
2021-07-28 08:39:00 +0200

    [NIXOS] fixed steam directx10 error

--- configurations/games.nix
+++ configurations/games.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
 
-let
-  steam = pkgs.unstable.steam.override {
-    extraLibraries = pkgs: with config.hardware.opengl;
-      if pkgs.hostPlatform.is64bit
-      then [ package ] ++ extraPackages
-      else [ package32 ] ++ extraPackages32;
-  };
-in
 {
+  programs.steam.enable = true;
+
   environment.systemPackages = with pkgs; [
     minecraft
-    steam
-    steam.run
   ];
 
   hardware = {

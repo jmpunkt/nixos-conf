d60de29e19176bc74ad01277c3cbb9a957b7b382
Jonas Meurer <jmpunkt@outlook.com>
2022-03-21 17:28:39 +0100

    [NIXOS] telnet package does not exist anymore

--- configurations/base.nix
+++ configurations/base.nix
@@ -1,19 +1,18 @@
-{ lib
-, config
-, pkgs
-, options
-, ...
-}:
 {
-  imports = import ( ../modules/all-nixos.nix ) ++ [ ./flakes.nix ];
+  lib,
+  config,
+  pkgs,
+  options,
+  ...
+}: {
+  imports = import (../modules/all-nixos.nix) ++ [./flakes.nix];
   nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    substituters = ["https://nix-community.cachix.org"];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
-  nixpkgs.overlays = [ ( import ../overlays/00-patch.nix ) ( import ../overlays/10-pkgs.nix ) ];
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  environment.systemPackages =
-    with pkgs; [ srm curl unzip htop telnet fd exa ripgrep git sdparm hdparm smartmontools pciutils usbutils ];
+  nixpkgs.overlays = [(import ../overlays/00-patch.nix) (import ../overlays/10-pkgs.nix)];
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+  environment.systemPackages = with pkgs; [srm curl unzip htop inetutils fd exa ripgrep git sdparm hdparm smartmontools pciutils usbutils];
   networking.firewall.enable = true;
   programs = {
     fish.enable = true;

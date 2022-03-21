1b17cec9d7ecd6a0f1c66c7c0984114022f9ebbe
Jonas Meurer <jmpunkt@outlook.com>
2022-03-21 17:29:39 +0100

    [PKGS] added missing packages for building

--- pkgs/misc/tsc-dyn/default.nix
+++ pkgs/misc/tsc-dyn/default.nix
@@ -1,68 +1,67 @@
-{ stdenv
-, fetchFromGitHub
-, rustPlatform
-, cask
-, emacs
-, llvmPackages
-, clang
-, pkgconfig
-}:
-let
+{
+  stdenv,
+  fetchFromGitHub,
+  rustPlatform,
+  cask,
+  emacs,
+  llvmPackages,
+  clang,
+  pkgconfig,
+  python,
+  which,
+}: let
   version = "0.15.1";
   src =
     fetchFromGitHub
-      {
-        owner = "emacs-tree-sitter";
-        repo = "elisp-tree-sitter";
-        rev = version;
-        sha256 = "WgkGtmw63+kRLTRiSEO4bFF2IguH5g4odCujyazkwJc=";
-      };
+    {
+      owner = "emacs-tree-sitter";
+      repo = "elisp-tree-sitter";
+      rev = version;
+      sha256 = "WgkGtmw63+kRLTRiSEO4bFF2IguH5g4odCujyazkwJc=";
+    };
   rust-version =
     rustPlatform.buildRustPackage
-      rec {
-        inherit src version;
-        name = "tree-sitter-cli-dyn-rust";
-        nativeBuildInputs = [ pkgconfig clang ];
-        LIBCLANG_PATH = "${ llvmPackages.libclang.lib }/lib";
-        cargoSha256 = "JXaAci41qJmS/xzyke3jTUZPSf2phdHEw23Qx4BVZFA=";
-      };
+    rec {
+      inherit src version;
+      name = "tree-sitter-cli-dyn-rust";
+      nativeBuildInputs = [pkgconfig clang];
+      LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+      cargoSha256 = "JXaAci41qJmS/xzyke3jTUZPSf2phdHEw23Qx4BVZFA=";
+    };
   em = (
     emacs.pkgs.withPackages
-      (
-        epkgs:
-        (
-          with epkgs.melpaStablePackages;
-          [
-            # cask
-            cask
-            s
-            f
-            commander
-            git
-            epl
-            shut-up
-            package-build
-            ansi
-            # package
-            tsc
-          ]
-        )
+    (
+      epkgs: (
+        with epkgs.melpaStablePackages; [
+          # cask
+          cask
+          s
+          f
+          commander
+          git
+          epl
+          shut-up
+          package-build
+          ansi
+          # package
+          tsc
+        ]
       )
+    )
   );
 in
-stdenv.mkDerivation
+  stdenv.mkDerivation
   rec {
     inherit version src;
     name = "tree-sitter-cli-dyn";
-    nativeBuildInputs = [ em ];
-    buildPhase =
-      ''
-      caskscript="${ em.deps }/bin/cask"
+    nativeBuildInputs = [em python which];
+    buildPhase = ''
+      caskscript="${em.deps}/bin/cask"
 
       export HOME=$(mkdir .home)
-      export EMACS="${ em }/bin/emacs"
+      export EMACS="${em}/bin/emacs"
 
-      ln -s ${ src } .
+      ln -s ${src} .
 
       mkdir -p "$(bash $caskscript package-directory)"
       # bash $caskscript link tsc core
@@ -70,14 +69,13 @@ stdenv.mkDerivation
 
       (
           cd core
-          cp -f "${ rust-version }/lib/libtsc_dyn.so" "tsc-dyn.so"
+          cp -f "${rust-version}/lib/libtsc_dyn.so" "tsc-dyn.so"
           bash $caskscript build
       )
-      '';
-    installPhase =
-      ''
+    '';
+    installPhase = ''
       mkdir -p $out/lib
       cp core/tsc-dyn.so $out/lib/
-      echo "${ version }".1 | tr -d $'\n' > $out/lib/DYN-VERSION
-      '';
+      echo "${version}".1 | tr -d $'\n' > $out/lib/DYN-VERSION
+    '';
   }

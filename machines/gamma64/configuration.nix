bdd090bd5cb70dd55cd880b557213fef807e1cb2
Jonas Meurer <jmpunkt@outlook.com>
2022-12-01 19:00:46 +0100

    [CONF] change power settings for laptop

--- machines/gamma64/configuration.nix
+++ machines/gamma64/configuration.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   options,
+  lib,
   ...
 }: {
   imports = [
@@ -18,7 +19,14 @@
       efi.efiSysMountPoint = "/boot/efi";
     };
     cleanTmpDir = true;
-    kernelParams = ["acpi_backlight=native" "ivrs_ioapic[32]=00:14.0" "intel_pstate=disable"];
+    kernelParams = [
+      "acpi_backlight=native"
+      "ivrs_ioapic[32]=00:14.0"
+      "intel_pstate=disable"
+      "initcall_blacklist=acpi_cpufreq_init"
+      "amd_pstate.shared_mem=1"
+    ];
+    kernelModules = ["amd-pstate"];
     initrd.luks.devices."root".allowDiscards = true;
   };
   fileSystems."/".options = ["noatime" "nodiratime" "discard"];
@@ -26,9 +34,18 @@
   hardware.cpu.amd.updateMicrocode = true;
   environment.systemPackages = with pkgs; [tlp powertop s-tui config.boot.kernelPackages.cpupower];
   services = {
+    power-profiles-daemon.enable = lib.mkForce false;
     tlp = {
       enable = true;
-      settings = {"RUNTIME_PM_BLACKLIST" = "05:00.3 05:00.4";};
+      settings = {
+        "RUNTIME_PM_BLACKLIST" = "05:00.3 05:00.4";
+        TLP_DEFAULT_MODE = "BAT";
+        CPU_BOOST_ON_AC = 1;
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+        PCIE_ASPM_ON_BAT = "powersupersave";
+      };
     };
     xserver.libinput.touchpad = {
       naturalScrolling = true;

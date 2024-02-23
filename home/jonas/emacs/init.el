e6f340e97ef359c24eb3732e8cb963593e5e6244
Jonas Meurer <jmpunkt@outlook.com>
2024-02-23 09:41:49 +0100

    [EMACS] use ripgrep for xref

--- home/jonas/emacs/init.el
+++ home/jonas/emacs/init.el
@@ -203,7 +203,8 @@ eglot (if available)."
         display-line-numbers-grow-only t
         auth-source-save-behavior nil
         uniquify-buffer-name-style 'forward
-        history-delete-duplicates t)
+        history-delete-duplicates t
+        xref-search-program 'ripgrep)
   (setq-default mode-line-format
                 '((:eval
                    (simple-mode-line-render

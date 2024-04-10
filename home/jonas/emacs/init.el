c354befbc523764d8d3c0b521b9fa2f0d218ec13
Jonas Meurer <jmpunkt@outlook.com>
2024-04-10 23:25:57 +0200

    [EMACS] erase all duplicates in history

--- home/jonas/emacs/init.el
+++ home/jonas/emacs/init.el
@@ -465,7 +465,7 @@ If the cursor is on the last promt, then we want to insert at the current positi
      filtered))
   (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
   (setq eshell-history-size 1000
-        eshell-hist-ignoredups t))
+        eshell-hist-ignoredups 'erase))
 
 (use-package compile
   :defer t

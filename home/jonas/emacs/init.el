1322f20a5d149e7fe1799d172c893f5157906624
Jonas Meurer <jmpunkt@outlook.com>
2024-05-23 19:38:57 +0200

    [EMACS] enable precison scrolling

--- home/jonas/emacs/init.el
+++ home/jonas/emacs/init.el
@@ -267,6 +267,7 @@ eglot (if available)."
                       :height 110)
   (set-face-attribute 'variable-pitch nil :inherit 'default :family 'unspecified)
   (set-face-attribute 'fixed-pitch nil :inherit 'default :family 'unspecified)
+  (pixel-scroll-precision-mode 1)
   (save-place-mode 1)
   (global-hl-line-mode 1)
   (global-so-long-mode 1)

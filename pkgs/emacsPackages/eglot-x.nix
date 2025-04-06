029ec8db274572001b53decfaca08a8cda72f0ef
Jonas Meurer <jmpunkt@outlook.com>
2025-04-06 18:43:40 +0200

    [PKGS] update eglot-x

--- pkgs/emacsPackages/eglot-x.nix
+++ pkgs/emacsPackages/eglot-x.nix
@@ -6,20 +6,20 @@
   melpaBuild,
   writeText,
 }: let
-  rev = "295c0309dc836966467c95867d1593f1376507b6";
+  rev = "5b5a56ddf8a087520f5e25a7343ec6503338e35b";
   pname = "eglot-x";
   owner = "nemethf";
 in
   melpaBuild {
     inherit pname;
-    version = "20220401.0";
+    version = "20250216.0";
 
     commit = rev;
 
     src = fetchFromGitHub {
       inherit rev owner;
       repo = pname;
-      sha256 = "sha256-G/jnEQRVo6xpBaW5cBrcAD03P65stgGMhTM21pxdNvE=";
+      sha256 = "sha256-6FY/Y5ahA82N0uJ0Dvb5e390UoVBEi8E8kKGfyknM/k=";
     };
 
     recipe = writeText "recipe" ''

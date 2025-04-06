d562139e1821a5c63044f1d447c05d09db3d7dbb
Jonas Meurer <jmpunkt@outlook.com>
2025-04-06 18:43:19 +0200

    [PKGS] remove verapdf

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,6 @@
   latex = callPackage ./misc/latex {};
   emacs = callPackage ./applications/emacs {};
   hunspellDicts = callPackage ./hunspellDicts {};
-  verapdf = callPackage ./verapdf {};
   oh-my-svg = libsForQt5.callPackage ./oh-my-svg {};
   html-to-svg = callPackage ./html-to-svg {};
 }

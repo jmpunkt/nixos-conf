c1a6e4012afbc46734acefb397ad60e6e3b408ff
Jonas Meurer <jmpunkt@outlook.com>
2025-05-14 22:17:19 +0200

    [PKGS] import Emacs without `callPackage`
    We have to import the package manually since we might want to override it at
    some point. If we use `callPackage` we can only override the arguments specified
    in `pkgs/applications/emacs/default.nix`. But we want to override the arguments
    in the original Emacs derivation.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,9 @@
 {
   callPackage,
+  emacs-overlay,
   libsForQt5,
 }: {
-  emacs = callPackage ./applications/emacs {};
+  emacs = import ./applications/emacs { inherit emacs-overlay; };
   hunspellDicts = callPackage ./hunspellDicts {};
   oh-my-svg = libsForQt5.callPackage ./oh-my-svg {};
   html-to-svg = callPackage ./html-to-svg {};

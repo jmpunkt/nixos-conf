2db3eebcd600ff3c03af04abf27272ba1be23afd
Jonas Meurer <jmpunkt@outlook.com>
2025-05-31 17:02:12 +0200

    [HOME] disable gtk config
    It is currently not possible for 25.05 to force the Gtk2 config. Therefore,
    home-manager will always fail when starting.

--- home/jonas/desktop/kde.nix
+++ home/jonas/desktop/kde.nix
@@ -1,17 +1,16 @@
 {pkgs, ...}: {
-  gtk = {
-    enable = true;
-    font.name = "Noto Sans";
-    font.size = 10;
-    iconTheme = {
-      package = pkgs.kdePackages.breeze-icons;
-      name = "breeze";
-    };
-    theme = {
-      package = pkgs.kdePackages.breeze-gtk;
-      name = "Breeze";
-    };
-  };
+  # gtk = {
+  #   enable = true;
+  #   font.name = "Noto Sans";
+  #   iconTheme = {
+  #     package = pkgs.kdePackages.breeze-icons;
+  #     name = "breeze";
+  #   };
+  #   theme = {
+  #     package = pkgs.kdePackages.breeze-gtk;
+  #     name = "Breeze";
+  #   };
+  # };
   qt = {
     enable = true;
     platformTheme.name = "kde6";

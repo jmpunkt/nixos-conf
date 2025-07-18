2d56301ba0b28389bc5bd0553c50f0d01b55d979
Jonas Meurer <jmpunkt@outlook.com>
2025-07-18 22:12:41 +0200

    [EMACS] remove use-package statistics with higher time precision

--- home/jonas/emacs/early-init.el
+++ home/jonas/emacs/early-init.el
@@ -15,24 +15,6 @@
       gc-cons-threshold (* 128 1024 1024))
 
 ;;; Packages
-(defun jmpunkt/use-package-statistics-convert (package)
-  "Return information about PACKAGE.
-
-The information is formatted in a way suitable for
-`use-package-statistics-mode'."
-  (let ((statistics (gethash package use-package-statistics)))
-    (list
-     package
-     (vector
-      (symbol-name package)
-      (use-package-statistics-status statistics)
-      (format-time-string
-       "%H:%M:%S.%6N"
-       (use-package-statistics-last-event statistics))
-      (format "%.4f" (use-package-statistics-time statistics))))))
-
-(advice-add 'use-package-statistics-convert :override #'jmpunkt/use-package-statistics-convert)
-
 (setq use-package-always-defer t)
 (setq package-archives nil)
 (setq package-enable-at-startup nil)

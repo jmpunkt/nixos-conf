6ba3cfb5c763421abb00cdb2570a795571cd0394
Jonas Meurer <jmpunkt@outlook.com>
2025-08-30 10:34:38 +0200

    [CONF] enable lutris

--- configurations/games.nix
+++ configurations/games.nix
@@ -4,60 +4,51 @@
   lib,
   ...
 }:
-let
-  # NOTE: do not enable wayland (currently it detects two monitors as
-  # one and launches all battle-net related with twice the width by
-  # same height.)
-  wine = pkgs.wineWowPackages.staging;
-  battle-net = pkgs.writeScriptBin "battle-net" ''
-    export WINEARCH=win64
-    export WINEPREFIX=$HOME/.wine-battlenet
-    export PATH="${pkgs.wineWowPackages.staging}/bin:${pkgs.winetricks}/bin:$PATH"
 
-    battleNet="$WINEPREFIX/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
-
-    if [[ $# -eq 0 ]]; then
-      if [ -f "$battleNet" ]; then
-        # HACK: fixes failed starting battle-net because of missing QT
-        find "$WINEPREFIX/drive_c/Program Files (x86)/Battle.net" -name "qwindows.dll" -exec setfattr -x user.DOSATTRIB "{}" \;
-        wine "$battleNet" "$@"
-      else
-        echo "Battl-net not installed. Run battle-net -i <PATH_TO_BATTLENT_INSTALLER>"
-        exit 1
-      fi
-    else
-      case "$1" in
-        install)
-          winetricks dxvk
-          wine "''${@:2}"
-        ;;
-        run)
-          wine "''${@:2}"
-        ;;
-        winetricks)
-          winetricks "''${@:2}"
-        ;;
-        *)
-          echo "only install or run allowed."
-          exit 1
-        ;;
-      esac
-    fi
-
-  '';
-in
 {
-  programs.firejail = {
-    wrappedBinaries = {
-      wine = {
-        executable = "${lib.getBin wine}/bin/wine";
-        profile = "${pkgs.firejail}/etc/firejail/wine.profile";
-      };
-    };
-  };
   programs.steam.enable = true;
   environment.systemPackages = with pkgs; [
+    bottles
+    (lutris.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          wineWowPackages.unstableFull
+          wineWowPackages.fonts
+          winetricks
+          dxvk
+          samba
+          vkd3d
+
+          pixman
+          libjpeg
+          vulkan-loader
+          vulkan-tools
+
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    })
+    wineWowPackages.unstableFull
+    wineWowPackages.fonts
+    winetricks
+
+    # wine dependencies
+    dxvk
     samba
-    battle-net
+    vkd3d
+    vulkan-loader
+    vulkan-tools
+    pixman
+    libjpeg
   ];
+
+  hardware.graphics.enable32Bit = true;
 }

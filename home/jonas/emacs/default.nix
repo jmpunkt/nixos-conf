88f3e3e11f15140f0abd5c5e7320f7bce83fc59c
Jonas Meurer <jmpunkt@outlook.com>
2025-09-03 21:07:52 +0200

    [HOME] move variable configuration into home config

--- home/jonas/emacs/default.nix
+++ home/jonas/emacs/default.nix
@@ -90,7 +90,6 @@
           jmpunktPkgs.typst-ts-mode
           jmpunktPkgs.copilot
           jmpunktPkgs.jj-mode
-          (jmpunktPkgs.nixosPaths (import ./variables.nix { inherit pkgs; }))
         ]
         ++ [
           (treesit-grammars.with-grammars (
@@ -98,5 +97,79 @@
           ))
         ]
       );
+
+    variables =
+      let
+        copilot = pkgs.nodePackages.jmpunkt."@github/copilot-language-server-1.364.0";
+        copilot-run = pkgs.writeShellScriptBin "copilot-run" ''
+          "${pkgs.nodejs}/bin/node" "${copilot}/lib/node_modules/@github/copilot-language-server/dist/language-server.js" "$@"
+        '';
+      in
+      {
+        org-plantuml-jar-path = "${pkgs.plantuml}/lib/plantuml.jar";
+        ob-mermaid-cli-path = "${pkgs.nodePackages.mermaid-cli}/bin/mmdc";
+        mermaid-mmdc-location = "${pkgs.nodePackages.mermaid-cli}/bin/mmdc";
+        languagetool-server-command = "${pkgs.languagetool}/share/languagetool-server.jar";
+        languagetool-console-command = "${pkgs.languagetool}/share/languagetool-commandline.jar";
+        languagetool-java-bin = "${pkgs.jre}/bin/java";
+        copilot-install-dir = "${copilot}";
+        copilot-server-executable = "${copilot-run}/bin/copilot-run";
+        copilot-version = copilot.version;
+        svg-lib-icon-collections = {
+          "\"material\"" = "file://${
+            pkgs.fetchFromGitHub {
+              owner = "Templarian";
+              repo = "MaterialDesign";
+              rev = "2424e748e0cc63ab7b9c095a099b9fe239b737c0";
+              sha256 = "sha256-QMGl7soAhErrrnY3aKOZpt49yebkSNzy10p/v5OaqQ0=";
+            }
+          }/svg/%s.svg";
+        };
+      };
+    paths =
+      let
+        core = with pkgs; [
+          fd # find file
+          ripgrep # search
+          pandoc # markdown, etc
+          git
+          git-absorb
+          universal-ctags
+          global
+        ];
+        lsp = with pkgs; [
+          ccls
+          yaml-language-server
+          nodePackages.typescript-language-server
+          unstable.nixd
+          unstable.tinymist # Typst
+          unstable.basedpyright
+          taplo # Toml
+          unstable.typespec
+        ];
+        org = with pkgs; [
+          # graphs
+          inkscape
+          imagemagick
+          graphviz-nox
+          pkgs.nodePackages.mermaid-cli
+          ghostscript
+          zip
+          tectonic
+          typst
+        ];
+        formatter = with pkgs; [
+          nixpkgs-fmt
+          pgformatter
+          nodePackages.prettier
+          nixfmt-rfc-style
+          typstyle
+          ruff
+          biome
+          (rustfmt.override { asNightly = true; })
+        ];
+      in
+      core ++ org ++ lsp ++ formatter;
+
   };
 }

a0c662cc8fa8faaf50f763970ad2fc1809e14df2
Jonas Meurer <jmpunkt@outlook.com>
2025-10-26 18:46:56 +0100

    [PKGS] update emacsPkgs.typst-ts-mode

--- pkgs/emacsPackages/typst-ts-mode.nix
+++ pkgs/emacsPackages/typst-ts-mode.nix
@@ -5,7 +5,7 @@
   writeText,
 }:
 let
-  rev = "972dc69d6b8a3f8983f6b8000654f59c8a8d05ba";
+  rev = "7c2ef0d5bd2b5a8727fe6d00938c47ba562e0c94";
   pname = "typst-ts-mode";
   owner = "meow_king";
 in
@@ -19,7 +19,7 @@ melpaBuild {
     inherit owner rev;
     domain = "codeberg.org";
     repo = pname;
-    sha256 = "sha256-+x8AthOxO0vBjaKSaiDpLnf+Ph0AqWl7EMei29dr85g=";
+    sha256 = "sha256-D+QEfEYlxJICcdUCleWpe7+HxePLSSmV7zAwvyTL0+Q=";
   };
 
   recipe = writeText "recipe" ''

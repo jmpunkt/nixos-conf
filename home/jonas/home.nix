caeee30965964aeb22179686c60eb420647b3a05
Jonas Meurer <jmpunkt@outlook.com>
2025-12-23 22:59:23 +0100

    [HOME] fix home-manager warning regarding deprecated ssh config

--- home/jonas/home.nix
+++ home/jonas/home.nix
@@ -28,7 +28,20 @@
   };
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       "qemu" = {
         controlPersist = "10m";
         controlMaster = "auto";

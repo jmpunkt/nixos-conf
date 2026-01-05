93df516e02e4d02e8e3b1105c8d2861b162423b8
Jonas Meurer <jmpunkt@outlook.com>
2026-01-05 20:42:02 +0100

    [PKG] use callPackge for Emacs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@
   libsForQt5,
 }:
 {
-  emacs = import ./applications/emacs { inherit emacs-overlay; };
+  emacs = callPackage ./applications/emacs { inherit emacs-overlay; };
   hunspellDicts = callPackage ./hunspellDicts { };
   oh-my-svg = libsForQt5.callPackage ./oh-my-svg { };
   html-to-svg = callPackage ./html-to-svg { };

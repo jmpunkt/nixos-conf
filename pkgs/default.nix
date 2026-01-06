f888e0b44abdf6bfa5f81d42dc3cbbb06ba0b873
Jonas Meurer <jmpunkt@outlook.com>
2026-01-06 18:04:04 +0100

    [PKGS] fix package import for Emacs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,9 @@
   libsForQt5,
 }:
 {
-  emacs = callPackage ./applications/emacs { inherit emacs-overlay; };
+  # NOTE: Use `import` to ensure that we can still override the resulting
+  # derivation.
+  emacs = import ./applications/emacs { inherit emacs-overlay; };
   hunspellDicts = callPackage ./hunspellDicts { };
   oh-my-svg = libsForQt5.callPackage ./oh-my-svg { };
   html-to-svg = callPackage ./html-to-svg { };

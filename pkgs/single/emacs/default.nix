29ef8604b34179893c07991f9350ab4d42c1ee4b
Jonas Meurer <jmpunkt@outlook.com>
2026-06-07 21:42:37 +0200

    [EMACS] remove custom patch for reading

--- pkgs/single/emacs/default.nix
+++ pkgs/single/emacs/default.nix
@@ -38,12 +38,7 @@
     {
       patches = builtins.trace "hello: ${toString patchesWithoutFix}" (
         patchesWithoutFix
-        ++ [
-          # keep an eye on (https://github.com/tyler-dodge/emacs/commit/e56c55a742c0f0d152afb4958b863cdf7207b4c3), not working for Linux since it
-          # is a MacOS workaround, patch below works fine and has
-          # taken from https://github.com/geza-herman/emacs/commit/784a9fd3d511b7f6794f713a8d0b1370ab1b2401
-          ./improved-reading.patch
-        ]
+        ++ []
       );
       env = (old.env or { }) // {
         CFLAGS = (
